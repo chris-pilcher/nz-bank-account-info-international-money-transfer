82b0514c989d50433e0e56fe4254cc216be15441
Chris Pilcher <7553472+chris-pilcher@users.noreply.github.com>
2019-06-11 16:09:27 +1200

    Prettier

--- test/main.test.js
+++ test/main.test.js
@@ -1,15 +1,15 @@
-const generate = require('../src/main');
+const generate = require("../src/main");
 
-test('throws exception when given invalid bank account number', () => {
-	expect(() => {
-		generate('44-1234-0012345-00');
-	}).toThrow();
+test("throws exception when given invalid bank account number", () => {
+  expect(() => {
+    generate("44-1234-0012345-00");
+  }).toThrow();
 });
 
-test('does not throw exception when given unknown bank account', () => {
-	expect(() => {
-		generate('01-902-0068389-00');
-	}).not.toThrow();
+test("does not throw exception when given unknown bank account", () => {
+  expect(() => {
+    generate("01-902-0068389-00");
+  }).not.toThrow();
 });
 
 // https://github.com/wytlytningNZ/NZ-Bank-Account-Validator/blob/master/test/library.spec.js
