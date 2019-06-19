6b6eb251e0d6bcd843b9e2d627e1e8d9c01eaa0a
Chris Pilcher <7553472+chris-pilcher@users.noreply.github.com>
2019-06-19 19:22:20 +1200

    Create Generator (#1)
    * Create placeholder
    
    * Add bank mapping
    
    * Add jest
    
    * src and test folders
    
    * add link to NZ-Bank-Account-Validator tests
    
    * Prettier
    
    * Add link to jestjs.io
    
    * Using test.each
    
    * Change to test.each(table). Remove console.log
    
    * Add beneficiaryAccount bsbSortCode and iban
    
    * Add test cases for each bank

---
+++ src/main.js
@@ -0,0 +1,85 @@
+const bankValidator = require("nz-bank-account-validator/lib/NZ-Bank-Account-Validator");
+
+let bankMapping = {
+  "01": {
+    name: "ANZ",
+    swift: "ANZBNZ22",
+    address:
+      "ANZ Bank New Zealand Ltd, Floor 10, 170-186 Featherston St, Wellington 6011, New Zealand"
+  },
+  "02": {
+    name: "BNZ",
+    swift: "BKNZNZ22",
+    address: "Bank of New Zealand, Wellington, New Zealand"
+  }, // Can also be "The Co-operative Bank" worry about this later
+  "03": {
+    name: "Westpac",
+    swift: "WPACNZ2W",
+    address: "Westpac New Zealand Limited, Auckland, New Zealand"
+  }, // Can also be NZ credit union/Rabobank
+  "06": {
+    name: "National Bank of New Zealand",
+    swift: "TODO",
+    address: "[TODO]"
+  }, // ?? Defunct??
+  "08": { name: "National Australia Bank", swift: "TODO", address: "[TODO]" }, // ??? Who is this ?
+  "10": {
+    name: "Industrial and Commercial Bank of China",
+    swift: "ICBKNZ2A",
+    address:
+      "Industrial and Commercial Bank of China Ltd, Level 22/188 Quay St, Auckland 1142, New Zealand"
+  },
+  "12": {
+    name: "ASB",
+    swift: "ASBBNZ2A",
+    address:
+      "ASB Bank Limited, 12 Jellicoe Street, Auckland Central, Auckland 1010, New Zealand"
+  },
+  "15": {
+    name: "TSB",
+    swift: "TSBANZ22",
+    address: "TSB Centre, 120 Devon St East, New Plymouth 4310, New Zealand"
+  },
+  "30": {
+    name: "HSBC",
+    swift: "HSBCNZ2A",
+    address:
+      "The Hongkong and Shanghai Banking Corporation Limited, HSBC House, Level 9, 1 Queen Street, Auckland, New Zealand"
+  },
+  "31": {
+    name: "Citibank",
+    swift: "CITINZ2X",
+    address: "CitiBank, Level 7/23 Customs St E, Auckland 1010, New Zealand"
+  },
+  "38": {
+    name: "Kiwibank",
+    swift: "KIWINZ22",
+    address:
+      "Kiwibank Limited, Ground Floor, New Zealand Post House, 7 Waterloo Quay, Wellington 6011, New Zealand"
+  }
+};
+
+function generate(accountNumber) {
+  // TODO: validate account number
+  bankValidator.validate(accountNumber);
+
+  const [bank, branch, account, suffix] = accountNumber.split("-"); // TODO: use bankValidator
+  const bankDetails = bankMapping[bank];
+  if (!bankDetails) {
+    throw `Error: Unknown bank ${bank}. Known banks are ${Object.keys(
+      bankMapping
+    )
+      .sort()
+      .map(bankKey => `${bankMapping[bankKey].name} (${bankKey})`)
+      .join(" ")}`;
+  }
+
+  return {
+    ...bankDetails,
+    beneficiaryAccount: `${bank}-${branch}-${account}-${suffix}`,
+    bsbSortCode: `${bank}-${branch}`,
+    iban: `${bank}-${branch}-${account}-${suffix}`
+  };
+}
+
+module.exports = generate;
