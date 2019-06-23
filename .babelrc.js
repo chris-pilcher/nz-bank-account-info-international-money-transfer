99c14f970a6cbef38fab69a4816956a862e9799c
Chris Pilcher <7553472+chris-pilcher@users.noreply.github.com>
2019-06-24 10:45:43 +1200

    Use plugin-transform-spread in webpack

--- .babelrc.js
+++ .babelrc.js
@@ -1,7 +1,3 @@
 const presets = [["@babel/preset-env", { targets: { node: "current" } }]];
-const plugins = ["@babel/plugin-transform-spread"];
 
-module.exports = {
-  plugins,
-  presets
-};
+module.exports = { presets };
